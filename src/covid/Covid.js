984ae067537a156d63c547e6babb204a526544e7
Eric Cote <eric@coteexpert>
2022-10-07 08:49:16 -0400

    Friday morning commit

---
+++ src/covid/Covid.js
@@ -0,0 +1,48 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { Button, Spinner, Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
+
+const formatNum = Intl.NumberFormat("en-US").format;
+
+function Covid() {
+  const countries = useLoaderData();
+
+  return (
+    <>
+      <h1>Covid cases per country</h1>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Flag</th>
+            <th>Country</th>
+            <th>Total Cases</th>
+            <th>Total Recovered</th>
+            <th>Total Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((c) => (
+            <tr key={c.country}>
+              <td>
+                <img
+                  src={c.countryInfo.flag}
+                  height="20"
+                  alt={`flag of ${c.country}`}
+                />
+              </td>
+              <td>{c.country}</td>
+              <td className="text-end">{formatNum(c.cases)}</td>
+              <td className="text-end">{formatNum(c.recovered)}</td>
+              <td className="text-end">{formatNum(c.active)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default Covid;
